50b3111d4ccfeda9db832302b4aebb3dedcbbaa3
Administrator <Administrator@PC231>
2020-06-04 15:05:58 +0700

    [MVC - BigSchool] Validate options

--- NguyenThanhHao_Lab456/Controllers/CoursesController.cs
+++ NguyenThanhHao_Lab456/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using NguyenThanhHao_Lab456.Models;
+﻿using Microsoft.AspNet.Identity;
+using NguyenThanhHao_Lab456.Models;
 using NguyenThanhHao_Lab456.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,8 @@ namespace NguyenThanhHao_Lab456.Controllers
             _dbContext = new ApplicationDbContext();
         }
         // GET: Courses
+        [Authorize]
+        [HttpPost]
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -23,6 +26,24 @@ namespace NguyenThanhHao_Lab456.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- NguyenThanhHao_Lab456/ViewModels/CourseViewModel.cs
+++ NguyenThanhHao_Lab456/ViewModels/CourseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using NguyenThanhHao_Lab456.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,13 @@ namespace NguyenThanhHao_Lab456.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime ()
