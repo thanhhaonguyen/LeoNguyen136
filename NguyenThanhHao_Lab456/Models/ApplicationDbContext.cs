f8f37a091af702e90148cb972a3b870f0a5303d8
Administrator <Administrator@PC231>
2020-06-04 13:56:47 +0700

    [MVC - BigSchool] Add in PopularCategoryTable

--- NguyenThanhHao_Lab456/Models/ApplicationDbContext.cs
+++ NguyenThanhHao_Lab456/Models/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,8 @@ namespace NguyenThanhHao_Lab456.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
