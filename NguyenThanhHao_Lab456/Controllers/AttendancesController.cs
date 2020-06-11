ed075a01604b7686be8ddc8914d945e801182191
LeoNguyen136 <leosnart1306@gmai.com>
2020-06-11 14:31:03 +0700

    [MVC - BigSchool] Add button handle event enroll courses

---
+++ NguyenThanhHao_Lab456/Controllers/AttendancesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using NguyenThanhHao_Lab456.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NguyenThanhHao_Lab456.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+                return BadRequest("The Attendace already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
